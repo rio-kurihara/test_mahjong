5515b803cef369675fc7ece7949a1d6287bbe62b
rio-kurihara <mahjiong.auto.point.calc@gmail.com>
2018-06-15 21:20:58 +0900

    test

--- app_with_handler.py
+++ app_with_handler.py
@@ -63,7 +63,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
-    text = hello_world():
+    text = hello_world()
     line_bot_api.reply_message(
         event.reply_token, TextSendMessage(text=text))
 
