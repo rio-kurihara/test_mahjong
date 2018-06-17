f5409e9c7a16f6c8fc4da0c90390ef6b1268f744
rio-kurihara <mahjiong.auto.point.calc@gmail.com>
2018-06-17 14:56:59 +0900

    test

--- app_with_handler.py
+++ app_with_handler.py
@@ -62,12 +62,12 @@ def callback():
 
     return 'OK'
 
-def make_image_message():
-    messages = ImageSendMessage(
-        original_content_url="/static/test.png", #JPEG 最大画像サイズ：240×240 最大ファイルサイズ：1MB(注意:仕様が変わっていた)
-        preview_image_url="/static/test.png" #JPEG 最大画像サイズ：1024×1024 最大ファイルサイズ：1MB(注意:仕様が変わっていた)
-    )
-    return messages
+# def make_image_message():
+#     messages = ImageSendMessage(
+#         original_content_url="/static/test.png", #JPEG 最大画像サイズ：240×240 最大ファイルサイズ：1MB(注意:仕様が変わっていた)
+#         preview_image_url="/static/test.png" #JPEG 最大画像サイズ：1024×1024 最大ファイルサイズ：1MB(注意:仕様が変わっていた)
+#     )
+#     return messages
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
@@ -77,9 +77,13 @@ def handle_text_message(event):
 
 @handler.add(MessageEvent, message=ImageMessage)
 def message_img(event):
-    messages = make_image_message()
+    # messages = make_image_message()
     line_bot_api.reply_message(
-        event.reply_token, messages)
+        event.reply_token,
+        ImageSendMessage(
+            original_content_url="test.png", #JPEG 最大画像サイズ：240×240 最大ファイルサイズ：1MB(注意:仕様が変わっていた)
+            preview_image_url="test.png" #JPEG 最大画像サイズ：1024×1024 最大ファイルサイズ：1MB(注意:仕様が変わっていた)
+        ))
 
 if __name__ == "__main__":
     arg_parser = ArgumentParser(
