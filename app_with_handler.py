813e4fb876d1c1d0d79bcf0f1c384a748433569c
rio-kurihara <mahjiong.auto.point.calc@gmail.com>
2018-06-17 14:25:12 +0900

    test

--- app_with_handler.py
+++ app_with_handler.py
@@ -64,8 +64,8 @@ def callback():
 
 def make_image_message():
     messages = ImageSendMessage(
-        original_content_url="https://test-mahjong.herokuapp.com/static/test.png", #JPEG 最大画像サイズ：240×240 最大ファイルサイズ：1MB(注意:仕様が変わっていた)
-        preview_image_url="https://test-mahjong.herokuapp.com/static/test.png" #JPEG 最大画像サイズ：1024×1024 最大ファイルサイズ：1MB(注意:仕様が変わっていた)
+        original_content_url="/static/test.png", #JPEG 最大画像サイズ：240×240 最大ファイルサイズ：1MB(注意:仕様が変わっていた)
+        preview_image_url="/static/test.png" #JPEG 最大画像サイズ：1024×1024 最大ファイルサイズ：1MB(注意:仕様が変わっていた)
     )
     return messages
 
