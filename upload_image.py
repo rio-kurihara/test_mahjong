f6f3a721e4cfbc5a5be15490c7944991040951de
rio-kurihara <mahjiong.auto.point.calc@gmail.com>
2018-06-19 11:48:11 +0900

    test

--- upload_image.py
+++ upload_image.py
@@ -1,28 +1,25 @@
-import os
-import sys
-sys.path.append('./')
-from argparse import ArgumentParser
-from flask import Flask, render_template, request
-from io import BytesIO
-import urllib
-import cv2
-import numpy as np
+from cStringIO import StringI
 
-app = Flask(__name__)
+from boto import connect_s3
 
-@app.route('/test.png')
-def index():
-    # 「templates/index.html」のテンプレートを使う
-    # 「message」という変数に"Hello"と代入した状態で、テンプレート内で使う
-    return render_template('index.html')
+# token
+access_key = ''
+secret_key = ''
 
-if __name__ == '__main__':
-    arg_parser = ArgumentParser(
-        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
-    )
-    arg_parser.add_argument('-p', '--port', default=8000, help='port')
-    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
-    options = arg_parser.parse_args()
-    port = int(os.environ.get('PORT', 55542))
+# bucket, key
+bucket_name = ''
+file_key = ''
 
-    app.run(debug=options.debug, port=port, host='0.0.0.0')
+# S3 からファイルを取得
+conn = connect_s3(access_key, secret_key)
+bucket = conn.get_bucket(bucket_name)
+key = bucket.get_key(file_key)
+
+# key_name で取得できなかった場合 None が返る
+if not key:
+    return u'ないよー'
+
+# ファイルダウンローして、 StgingIO に書き込み
+fp = StringIO()
+key.get_contents_to_file(fp)
+fp.seek(0)
