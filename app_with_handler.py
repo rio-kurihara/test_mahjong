9d828808a9ae28aa7cacf7a58656b51b91d80ac4
rio-kurihara <mahjiong.auto.point.calc@gmail.com>
2018-07-01 17:39:11 +0900

    test return image

--- app_with_handler.py
+++ app_with_handler.py
@@ -33,8 +33,8 @@ from linebot.models import (
 
 app_name = "test-mahjong"
 
-# os.makedirs("./static/images")
-app = Flask(__name__, static_url_path="/static")
+os.makedirs("./static/images")
+app = Flask(__name__, static_url_path="./static")
 
 # get channel_secret and channel_access_token from your environment variable
 channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
