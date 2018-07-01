5cb6288197c7450f09f4ff80e879dcf86d6a86aa
rio-kurihara <mahjiong.auto.point.calc@gmail.com>
2018-07-01 14:57:30 +0900

    test return image

--- app_with_handler.py
+++ app_with_handler.py
@@ -28,7 +28,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, ImageMessage
+    MessageEvent, TextMessage, TextSendMessage, ImageMessage, ImageSendMessage
 )
 
 app_name = "test-mahjong"
