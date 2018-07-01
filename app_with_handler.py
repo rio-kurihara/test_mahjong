1873576be87c1afe57e3775d924928cf1dfd0ade
rio-kurihara <mahjiong.auto.point.calc@gmail.com>
2018-07-01 17:58:58 +0900

    test return image

--- app_with_handler.py
+++ app_with_handler.py
@@ -33,8 +33,8 @@ from linebot.models import (
 
 app_name = "test-mahjong"
 
-# os.makedirs("/app/static/images")
-app = Flask(__name__, static_url_path="/app/static")
+# os.makedirs("app/static/images")
+app = Flask(__name__, static_url_path="app/static")
 
 # get channel_secret and channel_access_token from your environment variable
 channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
@@ -86,7 +86,7 @@ def message_image(event):
         token = event.reply_token
         msg_id = event.message.id
         msg_content = line_bot_api.get_message_content(msg_id)
-        tmp_path = "/app/static/images/{}".format(msg_id)
+        tmp_path = "app/static/images/{}".format(msg_id)
 
         with open(tmp_path, "wb") as fw:
             for chunk in msg_content.iter_content():
@@ -101,9 +101,6 @@ def message_image(event):
 
             img_msg = ImageSendMessage(original_content_url=url, preview_image_url=url)
             line_bot_api.reply_message(event.reply_token, img_msg)
-            # line_bot_api.reply_message(
-            #     event.reply_token,
-            #     TextSendMessage(text=tmp_path))
 
         # sid = choose_source_id(event.source)
         # line_bot_api.push_message(sid, TextSendMessage(text="message_id: {}, {}, {}".format(msg_id, img_fmt, str(exif_table))))
