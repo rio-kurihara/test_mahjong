7e5608b0387909c2dd9843166e0696007e075750
rio-kurihara <mahjiong.auto.point.calc@gmail.com>
2018-07-01 14:43:26 +0900

    test return image

--- app_with_handler.py
+++ app_with_handler.py
@@ -69,44 +69,44 @@ def handle_text_message(event):
     text = hello_world()
     line_bot_api.reply_message(
         event.reply_token, TextSendMessage(text=text))
-
-
+#
+#
 # @handler.add(MessageEvent, message=ImageMessage)
 # def message_img(event):
 #     line_bot_api.reply_message(
 #         event.reply_token,
 #         TextSendMessage(text='たしかに画像だね、でもまだ受け付けてないんだ'))
 
-#
-# @handler.add(MessageEvent, message=ImageMessage)
-# def message_image(event):
-#     try:
-#         token = event.reply_token
-#         msg_id = event.message.id
-#         msg_content = line_bot_api.get_message_content(msg_id)
-#         tmp_path = "static/images/{}".format(msg_id)
-#
-#         with open(tmp_path, "wb") as fw:
-#             for chunk in msg_content.iter_content():
-#                 fw.write(chunk)
-#
-#         with Image.open(tmp_path) as img:
-#             img_fmt = img.format
-#
-#             if img_fmt == "JPEG":
-#                 os.rename(tmp_path, tmp_path + ".jpg")
-#                 url = "https://{}.herokuapp.com/{}.jpg".format(app_name, tmp_path)
-#
-#             img_msg = ImageSendMessage(original_content_url=url, preview_image_url=url)
-#             line_bot_api.reply_message(event.reply_token, img_msg)
-#
-#         sid = choose_source_id(event.source)
-#         line_bot_api.push_message(sid, TextSendMessage(text="message_id: {}, {}, {}".format(msg_id, img_fmt, str(exif_table))))
-#     except linebot.exceptions.LineBotApiError as e:
-#         # なんかエラー処理
-#         line_bot_api.reply_message(
-#             event.reply_token,
-#             TextSendMessage(text='Error'))
+
+@handler.add(MessageEvent, message=ImageMessage)
+def message_image(event):
+    try:
+        token = event.reply_token
+        msg_id = event.message.id
+        msg_content = line_bot_api.get_message_content(msg_id)
+        tmp_path = "./static/images/{}".format(msg_id)
+
+        with open(tmp_path, "wb") as fw:
+            for chunk in msg_content.iter_content():
+                fw.write(chunk)
+
+        with Image.open(tmp_path) as img:
+            img_fmt = img.format
+
+            if img_fmt == "JPEG":
+                os.rename(tmp_path, tmp_path + ".jpg")
+                url = "https://{}.herokuapp.com/{}.jpg".format(app_name, tmp_path)
+
+            img_msg = ImageSendMessage(original_content_url=url, preview_image_url=url)
+            line_bot_api.reply_message(event.reply_token, img_msg)
+
+        sid = choose_source_id(event.source)
+        line_bot_api.push_message(sid, TextSendMessage(text="message_id: {}, {}, {}".format(msg_id, img_fmt, str(exif_table))))
+    except linebot.exceptions.LineBotApiError as e:
+        # なんかエラー処理
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='Error'))
 
 if __name__ == "__main__":
     arg_parser = ArgumentParser(
