a0fe1fe52361e991814669ee2d06213b364500b6
rio-kurihara <mahjiong.auto.point.calc@gmail.com>
2018-09-05 19:26:37 +0900

    aaa

--- app_with_handler.py
+++ app_with_handler.py
@@ -33,7 +33,7 @@ from linebot.models import (
 
 app_name = "test-mahjong"
 
-# os.makedirs("app/static/images")
+os.makedirs("/static/")
 app = Flask(__name__, static_url_path="/static/")
 
 # get channel_secret and channel_access_token from your environment variable
