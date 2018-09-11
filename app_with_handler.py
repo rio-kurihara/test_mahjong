78c95ad7db078a8b474bd1794dc1cdb1abfbcb45
rio-kurihara <mahjiong.auto.point.calc@gmail.com>
2018-09-11 19:36:10 +0900

    add return two message

--- app_with_handler.py
+++ app_with_handler.py
@@ -110,7 +110,7 @@ def message_image(event):
             img_msg = ImageSendMessage(original_content_url=url, preview_image_url=url)
 #             line_bot_api.reply_message(event.reply_token, img_msg)
             txt_msg = TextSendMessage(text='ok')
-            line_bot_api.reply_message(event.reply_token, [txt_msg, txt_msg])
+            line_bot_api.reply_message(event.reply_token, txt_msg)
 
     # except:
     #     line_bot_api.reply_message(
